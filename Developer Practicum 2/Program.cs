29159e73a571819cc63f908dca13b8ec8826c297
lnfe <lnfe@DEPC008350>
2020-05-27 11:29:16 +0200

    added List

--- Developer Practicum 2/Program.cs
+++ Developer Practicum 2/Program.cs
@@ -1,62 +1,120 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Developer_Practicum_2
 {
     class Program
     {
+        protected class Dishes
+        {
+            public string dishtype {get; set;}
+        }
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
+
             string output = "" ;
+
+            string[] outputarray = new string[4];
+            int arraycounter = 0;
+            int dishcount1 = 0;
+            int dishcount2 = 0;
+            int dishcount3 = 0;
+            int dishcount4 = 0;
+
+            List<Dishes> dishlist = new List<Dishes>();
+
             var inputarray = input.Split(',');
+
+            
 
             if (inputarray[0] == "morning")
             {
                 inputarray.Skip(1);
+                foreach (string item in inputarray)
+                {
+                    dishlist.Add(new Dishes {
+                        dishtype = item
+                    });
+                }
+
+                var result = dishlist.GroupBy<dishtype>;
+
+                // group array
+
                 foreach (var item in inputarray)
                 {
                     switch (item)
                     {
                         case "1":
-                            output = output + ", Eggs";
-                        break;
+                            //output = "Eggs";
+                            outputarray[arraycounter] = "Eggs";
+                            arraycounter++;
+                            dishcount1++;
+                            break;
 
                         case "2":
-                            output = output + ", Toast";
-                        break;
+                            //output = "Toast";
+                            outputarray[arraycounter] = "Toast";
+                            arraycounter++;
+                            dishcount2++;
+                            break;
 
                         case "3":
-                            output = output + ", Coffee";
-                        break;
+                            //output = "Coffee";
+                            outputarray[arraycounter] = "Coffee";
+                            arraycounter++;
+                            dishcount3++;
+                            break;
 
                         case "4":
-                            output = output + ", Error";
-                        break;
+                            //output = "Error";
+                            outputarray[arraycounter] = "Error";
+                            arraycounter++;
+                            dishcount4++;
+                            break;
                     }
                 }
             }
             else if (inputarray[0] == "night")
             {
                 inputarray.Skip(1);
+
+                //group array
+
                 foreach (var item in inputarray)
                 {
                     switch (item)
                     {
                         case "1":
-                            output = output + ", Steak";
+                            //output = "Steak";
+                            outputarray[arraycounter] = "Steak";
+                            arraycounter++;
+                            dishcount1++;
                             break;
 
                         case "2":
-                            output = output + ", Potato";
+                            //output = "Potato";
+                            outputarray[arraycounter] = "Potato";
+                            arraycounter++;
+                            dishcount2++;
                             break;
 
                         case "3":
-                            output = output + ", Wine";
+                            //output = "Wine";
+                            outputarray[arraycounter] = "Wine";
+                            arraycounter++;
+                            dishcount3++;
                             break;
 
                         case "4":
-                            output = output + ", Cake";
+                            //output = "Cake";
+                            outputarray[arraycounter] = "Cake";
+                            arraycounter++;
+                            dishcount4++;
                             break;
                     }
                 }
@@ -66,7 +124,7 @@ namespace Developer_Practicum_2
                 Console.WriteLine("No available menu");
             }
 
-            Console.WriteLine(output);
+            Console.WriteLine(String.Join(", ",outputarray));
         }
     }
 }
